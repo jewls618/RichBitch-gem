7e69628fe7e66b67c7359054b2644d6fc4fda5b0
Julie Graceffa <julie.graceffa@gmail.com>
2016-09-03 15:50:21 -0400

    Update rich_bitch.rb

--- lib/rich_bitch.rb
+++ lib/rich_bitch.rb
@@ -1,5 +1,10 @@
 require "rich_bitch/version"
 
 module RichBitch
-  # Your code goes here...
+  def self.process(user_input)
+    split_string = user_input.downcase.split('')
+    final = split_string.each {|letter| letter.sub!('s', '$')}
+    return final.join('')
+  end
 end
+
